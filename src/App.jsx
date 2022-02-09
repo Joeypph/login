db204117333903fe682eaa7eca101aed885060e3
Joeypph <joseant_1098@hotmail.com>
2022-02-09 11:52:16 -0600

    Routes were created, and route of Menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import {BrowserRouter as Router, Routes,Route} from 'react-router-dom'
+import Inicio from './components/Inicio'
+import Admin from './components/Admin'
+import Login from './components/Login'
+import Menu from './components/Menu';
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="container">
+      <Router>
+        <Menu/>
+        <Routes>
+          <Route exact path='/' element={<Inicio/>}/>
+          <Route exact path='/admin' element={<Admin/>}/>
+          <Route exact path='/login' element={<Login/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
