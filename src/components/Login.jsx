473239bbb12797bc4c94dfe32e266c8ea158247e
Joeypph <joseant_1098@hotmail.com>
2022-02-09 12:22:26 -0600

    Created form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,8 +2,27 @@ import React from 'react'
 
 function Login() {
   return (
-    <div>
-        <h2>Login</h2>
+    <div className='row '>
+      <div className='col'></div>
+      <div className='col' style={{textAlign:'center'}}>
+          <form className='form-group' action="">
+             <input
+              className='form-control mt-4'
+              placeholder='Introduce el Email'
+              type="text"/>
+             <input
+              className='form-control mt-4'
+              placeholder='Introduce la Contraseña'
+              type="text"/>
+             <input
+              className='btn btn-dark mt-4' 
+              placeholder='Introduce la Contraseña'
+              type="submit"
+              value="Registrar Usuario"/>
+
+          </form>
+      </div>
+      <div className='col'></div>
     </div>
   )
 }
