890a12742c4be8554632f98b5de49345ce7aa7a8
Joeypph <joseant_1098@hotmail.com>
2022-02-09 12:57:23 -0600

    created UserRegistration into firebase

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,19 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {auth,createUserWithEmailAndPassword} from '../firebaseconfig'
 
 function Login() {
+
+  const[email,setEmail] = useState('')
+  const[password,setPassword] = useState('')
+
+  const registrarUsuario = (e) =>{
+    e.preventDefault()
+    try{
+      createUserWithEmailAndPassword(auth,email,password)
+      alert('Se ha creado el usuario')
+      setEmail('')
+      setPassword('')
+    }catch(e){
+      console.log(e);
+    }
+  }
   return (
     <div className='row '>
       <div className='col'></div>
       <div className='col' style={{textAlign:'center'}}>
-          <form className='form-group' action="">
+          <form className='form-group' onSubmit={registrarUsuario}>
              <input
+              onChange={(e)=>{setEmail(e.target.value)}}
               className='form-control mt-4'
               placeholder='Introduce el Email'
               type="text"/>
+
              <input
+              onChange={(e)=>{setPassword(e.target.value)}}
               className='form-control mt-4'
               placeholder='Introduce la Contraseña'
-              type="text"/>
+              type="password"/>
+              
              <input
               className='btn btn-dark mt-4' 
               placeholder='Introduce la Contraseña'
